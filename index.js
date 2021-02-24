f4ccd9ad9e6ab7e90a50fb199e67b8543c469ebe
Jaskomal Randhawa <jaskomal2@gmail.com>
2021-02-23 22:54:18 -0600

    done

--- index.js
+++ index.js
@@ -5,13 +5,32 @@ console.log("Here's your header:", header)
 
 /***** Deliverable 2 *****/
 
-
+header.style.color = 'red';
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
+ console.log('This is what the traveler object looks like: ', traveler)
 
+ const person = document.querySelector('#profile');
+ const image = person.querySelector('img');
+ const head2 = person.querySelector('h2');
+ const em = person.querySelector('em');
+
+ image.src = traveler.photo;
+ head2.innerText = traveler.name;
+ em.innerText = traveler.nickname;
+ //innerText, textContent, innerHtml
+
+ console.log(person);
 
 /***** Deliverable 4 *****/
+
+const animal = document.querySelector('#animals');
+const para = animal.querySelector('p');
+const imge = animal.querySelector('img');
+const linky = animal.querySelector('a');
+
+imge.src = animal.photo;
+para.innerText = animal.species;
 
 
 
